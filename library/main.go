4c898288d96e12851ad5ab261ba733a1399dfdcc
Elbek <twobealfa@yandex.ru>
2023-03-28 03:10:20 +0300

    fixed err with signup_page/added better err handle

--- library/main.go
+++ library/main.go
@@ -4,7 +4,10 @@ import (
 	//"database/sql"
 	"fmt"
 	"html/template"
+	"io"
 	"net/http"
+
+	"github.com/sirupsen/logrus"
 	//_ "github.com/go-sql-driver/mysql"
 )
 
@@ -16,10 +19,14 @@ func home_page(w http.ResponseWriter, r *http.Request) {
 	tmpl.ExecuteTemplate(w, "home_page", nil)
 }
 func signup_page(w http.ResponseWriter, r *http.Request) {
+
 	tmpl, err := template.ParseFiles("templates/signup_page.html", "templates/header.html", "templates/footer.html")
 	if err != nil {
-		fmt.Fprintf(w, err.Error())
+		logrus.Warnf("Got error in [handler], err: %v", err)
+		_, _ = io.WriteString(w, fmt.Sprintf("Error: %v", err))
+		return
 	}
+
 	tmpl.ExecuteTemplate(w, "signup_page", nil)
 }
 func handleRequest() {
